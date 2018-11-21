ad38fa483ec233d3b629714be90a5f7a4d23a614
Alexandr Vetrov <vetrov@icc.ru>
2018-11-21 23:49:07 +0800

    step-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
 import { MatCardModule, MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
-import { FormsComponent } from './forms/forms.component';
-import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormsComponent,
-    ReactiveFormsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
     MatCardModule,
     MatButtonModule
   ],
--- src/app/forms/forms.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-forms-comp',
-  templateUrl: './forms.component.html',
-})
-export class FormsComponent {
-  name = { first: 'Илья ', last: 'Муромец' };
-}
